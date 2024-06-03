50f4012102743d0e51b731c61c0f4da419934f23
LogiqueClergyman <adityavishwanathanverma@gmail.com>
2024-06-03 23:44:41 +0530

    to deploy client

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,11 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL =
+  "https://movie-plylistserver-9xoi053co-logiqueclergymans-projects.vercel.app/api/auth";
 
 const register = async (username, password) => {
-  const response = await axios.post(`${API_URL}/register`, { username, password });
+  const response = await axios.post(`${API_URL}/register`, {
+    username,
+    password,
+  });
   if (response.data.token) {
-    localStorage.setItem('user', response.data.token);
+    localStorage.setItem("user", response.data.token);
   }
   return response.data;
 };
@@ -13,17 +17,17 @@ const register = async (username, password) => {
 const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
   if (response.data.token) {
-    localStorage.setItem('user', response.data.token);
+    localStorage.setItem("user", response.data.token);
   }
   return response.data;
 };
 
 const logout = () => {
-  localStorage.removeItem('user');
+  localStorage.removeItem("user");
 };
 
 const getCurrentUser = () => {
-  return (localStorage.getItem('user'));
+  return localStorage.getItem("user");
 };
 
 export default {
--- client/src/services/movieListService.js
+++ client/src/services/movieListService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://movie-plylistserver-lhu1lzti5-logiqueclergymans-projects.vercel.app/api"; // Replace with your actual backend URL
+const API_BASE_URL = "https://movie-plylistserver-9xoi053co-logiqueclergymans-projects.vercel.app/api"; // Replace with your actual backend URL
 
 const getToken = () => {
   // Retrieve the token from local storage or any other secure storage
